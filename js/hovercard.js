81a713cdbf6947cef4e860374fa1ecae52ebde7a
Kyle Wagner <tskazion@gmail.com>
2015-12-18 03:48:02 +0000

    Adds a real hovercard.

--- js/hovercard.js
+++ js/hovercard.js
@@ -4,12 +4,93 @@
  */
 
 /* global isUserLink */
+var g_rtes_storedUserData = new Array();
+var g_rtes_token = "";
 
 // --- Functions
+
+function populateUserHovercard(hovercard, user) {
+    var cardContent = $(hovercard).find('.card-content');
+    cardContent.empty();
+    
+    // --- Header
+    var header = $('<div>').addClass('rtes-card-header');
+    cardContent.append(header);
+    
+    // --- Inner
+    var innerContent = $('<div>').addClass('row');
+    var userImage = $('<div>')
+            .addClass('user-image')
+            .addClass('col-xs-4')
+            .append($('<img>')
+                .addClass('img-thumbnail')
+                .attr('src', user.image));
+            
+    innerContent.append(userImage);
+    
+    var profileInfo = $('<div>')
+            .addClass('user-profile')
+            .addClass('col-xs-8')
+            .append(user.profileControls);
+            
+    innerContent.append(profileInfo);
+    
+    cardContent.append(innerContent);
+    
+    // --- Footer
+    var footer = $('<div>').addClass('rtes-card-footer row').html(g_rtes_token);
+    cardContent.append(footer);
+    
+    hovercard.data("loaded", true);
+}
+/* 
+ * Loads the user data into the card
+ */
+function loadUserData(hovercard, href) {
+    // --- Already have this data?
+    if (hovercard.data("loaded")) {
+        return;
+    }
+    
+    // --- See if the data is stored
+    var user = href.substring(href.lastIndexOf('/') + 1);
+    if(!g_rtes_storedUserData[user]) {
+        
+        // --- Get the data from the server
+        $.get(href, function(data){
+            // --- Parse the data into an object
+            var userProfile = new Array();
+            
+            // Profile Image
+            userProfile.image = $(data).find('.profile-img-lg').find('img.profile').attr('src');
+            
+            // Controls
+            userProfile.profileControls = $(data).find('.left-sidebar > .details');
+            console.log(userProfile.profileControls);
+            
+            // --- Add to global
+            g_rtes_storedUserData[user] = userProfile;
+            populateUserHovercard(hovercard, g_rtes_storedUserData[user]);
+            
+            // --- Find the token
+            $(data).find('form > input').each(function(){
+                if ( $(this).attr('name') === '_token' ) {
+                    g_rtes_token = $(this).attr('value');
+                } 
+            });
+        });
+        
+    } else {
+        populateUserHovercard(hovercard, g_rtes_storedUserData[user]);
+    }
+}
+
 /*
  * Wrap the anchor in a container for a link
  */
 function attachCard(anchor){
+    
+    // --- We set up a wrapper so we can have a hover card.
     var rtesCardWrapper = $('<span class="rtes-cardWrapper"></span>');
     
     $(rtesCardWrapper).on({
@@ -50,6 +131,7 @@ function attachCard(anchor){
             });
             
             hovercard.show();
+            loadUserData(hovercard, anchor.href);
         },
         mouseleave: function () {
             $('.rtes-hovercard').removeClass('top bottom');
@@ -58,8 +140,10 @@ function attachCard(anchor){
         }
     });
     
+    // --- Wrap the anchor in the wrapper
     $(anchor).wrap(rtesCardWrapper);
     
+    // --- Set up the card
     var rtesHovercard = $('<div class="rtes-hovercard"><div class="card-content"></div></div>');
     rtesHovercard.hide();
     rtesHovercard.find('.card-content').append('<div class="loading"><i class="fa fa-cog fa-spin fa-4x"></i></div>');
@@ -67,6 +151,10 @@ function attachCard(anchor){
     $(anchor).after(rtesHovercard);
 
 }
+
+/**
+ * The main function
+ */
 function main(){
     
     // Get all anchors
